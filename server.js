1d987a519e7b88d82f6d141661efcc155383da32
Alabere-coder <alabere4real247@gmail.com>
2022-12-28 15:03:08 +0100

    updates

--- server.js
+++ server.js
@@ -4,3 +4,12 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+
+const textBtns = document.querySelectorAll('.contact__btn');
+
+textBtns.forEach(textBtn => {
+    let text = textBtn.querySelector('p');
+
+    text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join('')
+})
